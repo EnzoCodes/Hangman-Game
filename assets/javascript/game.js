03bdf7a72ccec1be5b3ac3b6a4c2f3e3428d1a16
Your Name <enzocnop@gmail.com>
2017-10-26 20:58:05 -0700

    not complete but im done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,8 @@
+// I think I approached this from the wrong perspective, especially the choosing the word bit.
+// I should have used a for loop to randomly select a word, loop through it to check against
+// the player's input... I think the way I did it is ok, but it's going to take longer than
+// I have to complete, and I learned some valuable lessons along the way.  
+
 
 // document.getElementById("game-display").innerHTML = results;
 
@@ -6,43 +11,74 @@ var wordlist = [ "apple", "piano", "wisdom", "groom", "modern", "perspective",
                  "lemon", "melon", "lime", "life", "taxes", "money", "death",
                  "five", "bark", "equip", "ship", "flip"]
 // Length of wordlist
-var len = wordlist.length;
+var lenRandWord = wordlist.length;
 
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
                 "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var blankSpace = ["_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"];
+var blankSpace;
+var blankArray = [];
 //blank array to fill with the word.
 
 
 var letterChoice; // User's input of letter, defined in function. Should push to lettersGuessed.
-var wordChoice; // Goin' Global.
-var wordSplit; // Makin' it Global.
+var wordChoice; // Computer's word selection.
+var wordSplit; // Array containing individual letters.
+var letterCount; //Number of letters in the wordSplit Array.
+var trigger = true;
 
-wordChoice = wordlist[Math.floor(Math.random() * len)];
-wordSplit = wordChoice.split('');
 
 var lettersGuessed = []; //Empty Array to fill with letters already guessed.
 
-
-
 // SETTING SCORE VARIABLES
 var win = 0;
-var lose = 0;
 var guesses = 12;
 
-     document.onkeyup = function(event) {
+// run for loop for length of word. Compare user guess with length of split word run for loop.
 
-         var letterChoice = event.key; {
-             letterChoice.toLowerCase();
-             letterChoice.push(lettersGuessed);
-             //Only runs game IF letter has not been guessed previously. Letter is valid character. Guesses are > 0.
-             if (guesses > 0 &&
-                 alphabet.indexOf(letterChoice) != -1 &&
-                 lettersGuessed.indexOf(lettersGuessed) == -1 ) {
-                //Game
-             }
+// compare userInput across word.length as a for loop.
+
+
+// Trap game inside document.onclick - getElementById(button); // or class.
+
+
+if (trigger = true) {
+    wordChoice = wordlist[Math.floor(Math.random() * lenRandWord)];
+    wordSplit = wordChoice.split('');
+    letterCount = wordSplit.length;
+        // Print Correct number of Blanks to Page.
+        // Below should be for loop. if word = CharAt(i)
+        if (blankSpace != letterCount) {
+            blankSpace = letterCount;
+            document.getElementById("spaces").innerHTML = blankArray.join(" _ ");
+    }   trigger = false;
+}
+
+
+    document.onkeyup = function(event) {
+        var letterChoice = event.key;
+        letterChoice.toLowerCase();
+        //Run game IF Guesses remain, Valid Letter, Not Duplicate Letter.
+        if (guesses > 0 && alphabet.indexOf(letterChoice) != -1 && lettersGuessed.indexOf(lettersGuessed) == -1 ) {
+            // Pushes chosen letter to Array. Puts Array on screen!
+            lettersGuessed.push(letterChoice);
+            document.getElementById("lettersGuess").innerHTML = lettersGuessed.join(" ");
+            // Win / Wrong Logic
+            for(i = 0; i<wordSplit; i++) {
+                if (letterChoice.indexOf(wordSplit) == -1){ //Wrong condition.
+                guesses--;
+
+            }
+        }
+
+            // Win.
+
+
+
+            }
      };
+
+    // Reset button?
 
 
 // Alphabet array
@@ -52,7 +88,6 @@ var guesses = 12;
 // Blank input array (??) -
 // (Shows letters as entered, getElementById(); somewhere.)
 
-// function 1.
 // computer pick randWord.
 // len of word displayed as ‘_’. Maybe show ‘_’ “hiding” letters.
 
